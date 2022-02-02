f435c93413d4ddbddf803d9299d2be6dad753e19
F <kaminoko101@gmail.com>
2022-02-02 16:48:42 +0630

    Add modal for new order

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 
 import * as Location from "expo-location";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { calculateMapCoords } from "../helpers";
+
+import { Feather } from "@expo/vector-icons";
 
 import Pusher from "pusher-js/react-native";
 
@@ -15,7 +24,14 @@ import {
 } from "../keys";
 
 const HomeScreen = () => {
+  const [hasOrder, setHasOrder] = useState(false);
+  const [restaurantLocation, setRestaurantLocation] = useState();
+  const [restaurantAddress, setRestaurantAddress] = useState("");
+  const [customerLocation, setCustomerLocation] = useState();
+  const [customerAddress, setCustomerAddress] = useState("");
   const [driverLocation, setDriverLocation] = useState();
+  const [customer, setCustomer] = useState();
+  const [isOrderModalVisible, setIsOrderModalVisible] = useState(false);
 
   const pusher = new Pusher(CHANNELS_APP_KEY, {
     authEndpoint: `${NGROK_URL}/pusher/auth`,
@@ -74,8 +90,13 @@ const HomeScreen = () => {
     })();
   }, []);
 
+  const acceptOrder = () => {};
+
+  const declineOrder = () => {};
+
   return (
     <View>
+      <Button title="Open" onPress={() => setIsOrderModalVisible(true)} />
       {driverLocation && (
         <MapView
           style={styles.mapView}
@@ -91,6 +112,51 @@ const HomeScreen = () => {
           )}
         </MapView>
       )}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={isOrderModalVisible}
+        onRequestClose={() => {
+          setIsOrderModalVisible(!isOrderModalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <TouchableOpacity
+              onPress={() => setIsOrderModalVisible(false)}
+              style={styles.close}
+            >
+              <Feather name="x" size={28} color="#fcbf49" />
+            </TouchableOpacity>
+            <View style={styles.orderDetails}>
+              <View style={styles.orderDetailsAddressContainer}>
+                <Text style={styles.orderHeading}>Pick Up</Text>
+                <Text style={styles.orderDetailsAddress}>
+                  {restaurantAddress}
+                </Text>
+              </View>
+              <View style={styles.orderDetailsAddressContainer}>
+                <Text style={styles.orderHeading}>Drop Off</Text>
+                <Text style={styles.orderDetailsAddress}>
+                  {customerAddress}
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity
+              onPress={acceptOrder}
+              style={[styles.button, styles.buttonAccept]}
+            >
+              <Text style={styles.buttonText}>Accept</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={declineOrder}
+              style={[styles.button, styles.buttonDecline]}
+            >
+              <Text style={styles.buttonText}>Decline</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -101,5 +167,54 @@ const styles = StyleSheet.create({
   mapView: {
     width: "100%",
     height: "100%",
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    width: "80%",
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  close: {
+    alignSelf: "flex-end",
+  },
+  orderDetails: {
+    alignSelf: "flex-start",
+  },
+  orderDetailsAddressContainer: {
+    margin: 10,
+  },
+  orderDetailsAddress: {
+    fontSize: 16,
+    paddingVertical: 10,
+  },
+  orderHeading: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  button: {
+    width: "80%",
+    height: 60,
+    margin: 10,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonDecline: {
+    backgroundColor: "red",
+  },
+  buttonAccept: {
+    backgroundColor: "green",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    textTransform: "uppercase",
   },
 });
